095dc1b17ac37d4e60784d70008c0ba5519b6687
Andrew Schlesinger <aschles4@uncc.edu>
2018-02-14 01:42:41 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,55 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+def guess(letter)
+  
+  if letter.nil? || letter.empty?
+    raise ArgumentError
+  end
+  
+  raise ArgumentError if letter =~ /\W/
+  
+  letter.downcase!
+  
+  if(@guesses.include? letter) || (@wrong_guesses.include? letter)
+    return false
+  else
+    if @word.include? letter 
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+  end
+end
+
+def word_with_guesses
+  
+  @word.gsub(/[^#{@guesses + '#'}]/, '-')
+  
+end
+
+def check_win_or_lose
+  
+  if @wrong_guesses.length >= 7
+    return :lose
+  elsif !(word_with_guesses.include? "-")
+    return :win
+  else 
+    return :play
+  end
+end
+  
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
