e6cf28c7edc1eb554898e0b71bf33f1ba94a3fb3
Andrew Schlesinger <aschles4@uncc.edu>
2018-02-18 02:28:08 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,7 +40,24 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    
+    begin 
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter"
+      end
+      
+      if @game.check_win_or_lose == :win
+        redirect '/win'
+      elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+      else
+        redirect '/show'
+      end
+    rescue ArgumentError
+      flash[:message] = "Your input was invalid"
     redirect '/show'
+    end
+    
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
